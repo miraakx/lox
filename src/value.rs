55d3fd4776a89d27542e0b76e02f71c3424a3b52
Ivan <ivan.nrd@gmail.com>
2024-07-30 23:35:29 +0200

    Aggiunta gestione equals per Value::Callable e Value::ClassInstance

--- src/value.rs
+++ src/value.rs
@@ -25,9 +25,19 @@ impl PartialEq for Value
             (Value::Number(left),        Value::Number(right))        => left == right,
             (Value::String(left), Value::String(right)) => { Rc::ptr_eq(&left, &right) || *left == *right  }
             (Value::Nil,                       Value::Nil)                        => true,
-            (Value::Callable(_), Value::Callable(_))                              => todo!(),
-            (Value::ClassInstance(_), Value::ClassInstance(_))                    => todo!(),
-            _                                                                     => false
+            (Value::ClassInstance(left), Value::ClassInstance(right))   => { Rc::ptr_eq(&left.declaration, &right.declaration) },
+            (Value::Callable(left), Value::Callable(right)) => {
+                match (left, right) {
+                    (Callable::Function(l, _), Callable::Function(r, _))           => { Rc::ptr_eq(&l, &r) },
+                    (Callable::InitFunction(l, _), Callable::InitFunction(r, _))   => { Rc::ptr_eq(&l, &r) },
+                    (Callable::Class(l, _), Callable::Class(r, _))                 => { Rc::ptr_eq(&l, &r) },
+                    (Callable::Clock, Callable::Clock)                              => { true },
+                    (Callable::AssertEq, Callable::AssertEq)                        => { true },
+                    (Callable::Str, Callable::Str)                                  => { true },
+                    _ => false
+                }
+            },
+            _   => false
         }
     }
 }
